812be82ee5b790ca21244b811a155c3802c2eb8c
mikolajkusa <mkonieczny@ithaca.edu>
2022-02-11 11:17:41 -0500

    Added tests for bank account and extends bank account to savings and checking

---
+++ src/main/java/edu/ithaca/dturnbull/bank/CheckingAccount.java
@@ -0,0 +1,10 @@
+package edu.ithaca.dturnbull.bank;
+
+public class CheckingAccount extends BankAccount{
+
+    public CheckingAccount(String email, double startingBalance) {
+        super(email, startingBalance);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
---
+++ src/main/java/edu/ithaca/dturnbull/bank/SavingsAccount.java
@@ -0,0 +1,10 @@
+package edu.ithaca.dturnbull.bank;
+
+public class SavingsAccount extends BankAccount {
+
+    public SavingsAccount(String email, double startingBalance) {
+        super(email, startingBalance);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
